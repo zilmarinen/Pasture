00dd610021d40b4ad126e06a6845f171950785fe
Zack Brown <zack@zackbrown.co.uk>
2022-01-22 12:03:21 +0000

    Remove all models

---
+++ Pasture/Views/EditorView.swift
@@ -0,0 +1,56 @@
+//
+//  EditorView.swift
+//
+//  Created by Zack Brown on 15/10/2021.
+//
+
+import SceneKit
+import SwiftUI
+
+struct EditorView: View {
+    
+    @ObservedObject var model: AppViewModel
+
+    var body: some View {
+        
+        switch model.viewState {
+            
+        case .exporting(let progress):
+            
+            VStack {
+                                    
+                Text("Exporting Foliage")
+                
+                ProgressView(progress)
+                .progressViewStyle(CircularProgressViewStyle(tint: .accentColor))
+            }
+            
+        case .editing:
+            
+            SceneView(scene: model.editorModel.scene,
+                      pointOfView: model.editorModel.scene.cameraJig,
+                      options: [.allowsCameraControl,
+                                    .autoenablesDefaultLighting],
+                      delegate: nil)
+                .toolbar {
+                    
+                    Menu {
+                        
+                        Toggle("Show wireframes", isOn: $model.editorModel.showWireframes)
+                        
+                    } label: {
+                        
+                        Label("Change view settings", systemImage: "slider.horizontal.3")
+                    }
+                    
+                    Divider()
+                    
+                    Button { model.export() } label: {
+                        
+                        Image(systemName: "square.and.arrow.up")
+                            .help("Export Tileset")
+                    }
+                }
+        }
+    }
+}
